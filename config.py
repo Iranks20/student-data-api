65b19ad9f7bf4bc11ab8aff04cf64fcfc763a420
Iranks20 <rankunda48@gmail.com>
2023-01-20 18:31:07 +0300

    database

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-DB_CONFIG = dict(username="root", password="", host="localhost", port=3306, db="student_data")
-# DB_CONFIG = dict(username="root", password="1234", host="localhost", port=3306, db="student_data")
+# DB_CONFIG = dict(username="root", password="", host="localhost", port=3306, db="student_data")
+DB_CONFIG = dict(username="root", password="1234", host="localhost", port=3306, db="student_data")
